f5000bdb8714e5f85656e6491cb632d22c6c8a3e
marhong2 <67293152+marhong2@users.noreply.github.com>
2021-12-23 17:00:19 -0800

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+export default class ToDo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      currText: "",
+      render: null
+    }
+    this.newTask = this.newTask.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.removeTask = this.removeTask.bind(this)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  newTask = () => {
+    if(this.state.currText != ""){
+      this.state.list.push(this.state.currText);
+      this.state.currText = "";
+      this.forceUpdate()
+    }
+  }
+  onChange = (event) => {
+    this.state.currText = event.target.value;
+    this.forceUpdate()
+  }
+  removeTask = (event, index) => {
+    this.state.list.splice(index, 1)
+    this.forceUpdate()
+  }
+
+  render() {
+    return  (
+      <div>
+        <center>
+          <div>
+          <b>Todo List</b>
+          </div>
+        <br/>
+        <input type="text" onChange={this.onChange} value={this.state.currText}></input>
+        <button onClick={this.newTask} value={this.state.currText}>Create Task</button>
+
+        {
+          this.state.list.map((task, index) =>{
+            return (
+              <div>
+                {task}
+                <button onClick={(event) => {
+                  this.removeTask(event, index)
+                }}>done</button>
+              </div>
+            )
+          })
+        }
+        </center>
+      </div>
+    )
+  }
 }
-
-export default App;
